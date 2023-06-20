cc9ef1fb43316e12bce5de6e6c00f80d9affd714
Mauricio <mauricio_taboo.97@outlook.com>
2023-06-20 05:32:03 -0600

    Merge Fix

--- Back50mas1/controller/talleres.js
+++ Back50mas1/controller/talleres.js
@@ -1,5 +1,4 @@
 const Talleres = require('../models/TalleresModel');
-const Talleres = require('../models/TalleresModel');
 const argon2 = require('argon2');
 const { QueryTypes } = require('sequelize');
 const db = require('../config/Database');
@@ -8,8 +7,7 @@ const getTestM = async  (req,  res) => {
     res.status(200).json({  msg:  'Test Talleres'  });
 }
 
-const getTalleres = async (req, res) => {
-    try {
+
 const getTalleres = async (req, res) => {
     try {
         const response = await Talleres.findAll({
@@ -18,16 +16,10 @@ const getTalleres = async (req, res) => {
         res.status(200).json(response);
     } catch (error) {
         res.status(500).json({ msg: error.message });
-    } catch (error) {
-        res.status(500).json({ msg: error.message });
-    }
+    } 
 }
 
 
-const createtaller = async (req, res) => {
-    const { codigo_taller, nombre, descripcion, periodo } = req.body;
-    console.log(req.body);
-    try {
 const createtaller = async (req, res) => {
     const { codigo_taller, nombre, descripcion, periodo } = req.body;
     console.log(req.body);
@@ -44,14 +36,10 @@ const createtaller = async (req, res) => {
         res.status(200).json({ msg: "Taller Creado exitosamente" });
     } catch (error) {
         res.status(400).json({ msg: error.message });
-        res.status(200).json({ msg: "Taller Creado exitosamente" });
-    } catch (error) {
-        res.status(400).json({ msg: error.message });
-    }
+}
 }
 
-const getTalleresByCorreo = async (req, res) => {
-    try {
+
 const getTalleresByCorreo = async (req, res) => {
     try {
         const response = await db.query(`SELECT codigo_taller, periodo, A.nombre as taller, B.nombre, B.apellidoPaterno, B.apellidoMaterno from talleres A, usuarios B WHERE A.correo = B.correo AND A.correo = '${req.params.correo}';`, { type: QueryTypes.SELECT });
